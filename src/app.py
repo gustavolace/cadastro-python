740586eb004a16ce0473d01afe93f2864b8a198e
Gustavo Lacerda <99536403+gustavolace@users.noreply.github.com>
2023-11-21 17:38:13 -0300

    multiprocessing ok

--- src/app.py
+++ src/app.py
@@ -1,15 +1,11 @@
 from flask import Flask
-from routes import rotas_bp
+from src.routes import rotas_bp
 
 def create_app():
     app = Flask(__name__)
-
     app.register_blueprint(rotas_bp)
+    app.run()
 
-    
 
-    return app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+
--- src/services/webview.py
+++ src/services/webview.py
@@ -1,10 +1,9 @@
 import webview
 
-def exibir_interface():
-    with open('src/views/pages/index.html', 'r', encoding='utf-8') as file:
+def  run_webview():
+    with open('src/views/pages/login/index.html', 'r', encoding='utf-8') as file:
         html_content = file.read()
 
-    webview.create_window('Minha Janela', html=html_content, width=800, height=600)
+    webview.create_window('Minha Janela', "https://pywebview.flowrl.com", width=800, height=600)
     webview.start(debug=True, gui='edge')
 
-exibir_interface()
