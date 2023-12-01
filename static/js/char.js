455919ee7e25611c668a95b400e0b2af38351aa1
gustavolace <lsbgustavo@gmail.com>
2023-12-01 19:39:53 -0300

    char hair and skin alteration

--- static/js/char.js
+++ static/js/char.js
@@ -1,13 +1,29 @@
 const corSamples = document.querySelectorAll(".cor-sample");
-let hairColor
+const skinSamples = document.querySelectorAll(".skin-sample");
 
-colorSelect(corSamples, hairColor)
+let hairColor;
+let skinColor;
 
-function colorSelect(corSample, selectColor) {
-    corSample.forEach((sample) => {
-      sample.addEventListener("click", () => {
-        selectColor = sample.getAttribute("data-cor");
-        console.log(`Cor selecionada: ${selectColor}`);
-      });
+colorSelect(corSamples, "hair");
+colorSelect(skinSamples, "skin");
+
+function colorSelect(color, type) {
+  color.forEach((sample) => {
+    sample.addEventListener("click", () => {
+      let selectColor = sample.getAttribute("data-cor");
+      console.log(`Cor selecionada para ${type}: ${selectColor}`);
+
+      if (type === "hair") {
+        hairColor = selectColor;
+      } else if (type === "skin") {
+        skinColor = selectColor;
+      }
+      let divImg = document.querySelector(".character-image");
+      const defaultHairColor = hairColor || "yellow";
+      const defaultSkinColor = skinColor || "bege";
+      divImg.innerHTML = `
+  <img src="/static/img/${defaultHairColor}-${defaultSkinColor}.png" alt="Imagem do personagem">
+    `;
     });
+  });
 }
