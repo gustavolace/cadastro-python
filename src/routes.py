97dd8f42052d84225421e3d599b8716719703f67
gustavolace <lsbgustavo@gmail.com>
2023-12-07 16:43:06 -0300

    user routes

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request, redirect, url_for
 from src.helpers.imgLinks import colorLinks
 
 rotas_bp = Blueprint('rotas', __name__, template_folder='../static/templates')
@@ -14,7 +14,47 @@ create_route('/signin', 'sign-in.html')
 create_route('/signup', 'sign-up.html')
 create_route('/header', 'header.html')
 create_route('/char', 'char.html')
-create_route('/charlist', 'charlist.html')
+""" create_route('/charlist', 'charlist.html') """
+
+personagens = {
+    1: {'id': 1, 'name': 'Personagem 1', 'str': 8, 'int': 5, 'user_id': 1},
+    2: {'id': 2, 'name': 'Personagem 2', 'user_id': 1},
+    3: {'id': 3, 'name': 'Personagem 3', 'str': 6, 'int': 7, 'user_id': 2},
+}
+
+usuarios = {
+    1: {'id': 1, 'nome': 'usuario1', 'senha': 'senha1'},
+    2: {'id': 2, 'nome': 'usuario2', 'senha': 'senha2'},
+}
+
+@rotas_bp.route('/charlist/<int:user_id>', endpoint='user')
+def user(user_id):
+    personagens_do_usuario = [personagem for personagem in personagens.values() if personagem.get('user_id') == user_id]
+    usuario = usuarios.get(user_id)
+    if usuario:
+        return render_template('charlist.html', personagens=personagens_do_usuario)
+    return "Usuário não encontrado", 404
+
+
+@rotas_bp.route('/login', methods=['POST'])
+def validation():
+    user_name = request.form['usuario']
+    senha = request.form['senha']
+
+    for usuario_id, usuario_info in usuarios.items():
+        if usuario_info['nome'] == user_name and usuario_info['senha'] == senha:
+            return redirect(url_for('rotas.user', id=usuario_id))
+    return "Credenciais inválidas. Tente novamente."
+
+
+
+@rotas_bp.route('/personagem/<int:id>')
+def exibir_personagem(id):
+    personagem = personagens.get(id)
+    if personagem:
+        return render_template('personagem.html', personagem=personagem)
+    else:
+        return "Personagem não encontrado", 404
 
 
 @rotas_bp.route('/img', methods=['GET'])
